a11962a023374e76ef604ffb6bb58fe61f1a2c6a
Sara M <sara.mitranca@gmail.com>
2024-10-19 08:34:36 +0300

    merge?

--- poocurs.cpp
+++ poocurs.cpp
@@ -1,2 +1,16 @@
 //polimorfism----> f(int a) si f(float a)
 //polimorfism---->f(int A, intB) si f(int A)
+//polimorfism si daca unul din parametrii e referinta si altul e pointer
+#include<iostream>
+using namespace std;
+void f0(int a) {
+	a = 90;
+
+}
+
+void fi1(int& r) {
+}
+void f2(int* p) {
+
+}
+
